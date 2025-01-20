0261ae5adfaa945993f9837759a04b893e67aae6
Prabin <prabinojha100@gmail.com>
2025-01-20 23:17:31 +1100

    redo heroku changes

--- node_modules/heroku/lib/lib/apps/generation.d.ts
+++
@@ -1,4 +0,0 @@
-import { APIClient } from '@heroku-cli/command';
-import { App } from '../types/fir';
-export declare function isFirApp(appOrName: App | string, herokuApi?: APIClient): Promise<boolean>;
-export declare function isCedarApp(appOrName: App | string, herokuApi?: APIClient): Promise<boolean>;
